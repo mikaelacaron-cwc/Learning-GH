b8685b355551788e91c8a52e47ee41771f7edba2
Mikaela Caron <mikaelacaron@gmail.com>
2021-01-01 22:19:28 -0500

    Add image

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("What's up, CodeCrew!")
-            .padding()
+        VStack {
+            Image(systemName: "figure.wave")
+            Text("What's up, CodeCrew!")
+                .padding()
+        }
     }
 }
 
